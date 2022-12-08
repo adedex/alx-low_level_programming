7a230b7aa73c7b9fa3df1f2bfcd54c84766ae355
adedex <adedex1@gmail.com>
2022-12-08 12:57:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints string to stdout.
+ * Description: Use puts instead of printf to print to stdout 
+ * Return: 0 ( If Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
