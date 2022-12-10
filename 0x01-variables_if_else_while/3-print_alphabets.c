2c2fb5a81e339f0bb7f7ec269d6f47b8de066dc2
adedex <adedex1@gmail.com>
2022-12-09 16:03:54 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: 1 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	putchar(alphabet);
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	putchar(alphabet);
+	putchat ('\n')
+	return (0);
+}
