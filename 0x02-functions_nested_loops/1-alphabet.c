e7ff8f40a05d549d261fa7de2a49e987a25185ee
adedex <adedex1@gmail.com>
2022-12-13 12:14:22 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: Print lower case alphabets using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a' ; lower_case <= 'z' ; lower-case++)
+	putchar (lower_case);
+	putchar ('\n');
+	retuen(0);
+}
