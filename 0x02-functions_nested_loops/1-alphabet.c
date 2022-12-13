31df4af38e0b6a44640f8f837e3b6a9b35d3458d
adedex <adedex1@gmail.com>
2022-12-13 12:49:33 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,8 @@ void print_alphabet(void)
 	char lower_case;
 
 	for (lower_case = 'a' ; lower_case <= 'z' ; lower_case++)
-	_putchar (lower_case);
-	_putchar ('\n');
-	return(0);
+	{
+		_putchar (lower_case);
+	}
+_putchar ('\n')
 }
