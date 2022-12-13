16afa0cb59331ad288b2ced5fa5de58e53ba83b4
adedex <adedex1@gmail.com>
2022-12-13 13:44:33 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lower case alphabets ten times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a, i;
+
+	for(i = 0; i <= 9; i++)
+	{
+		for(a = 'a'; a <= 'z'; a++)
+		{
+			_putchar (a;)
+		}
+		_putchar (\'n');
+	}
+}
