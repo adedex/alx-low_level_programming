7b113b52653cfb55bc52fb769c5cf09a91510fd1
adedex <adedex1@gmail.com>
2022-12-15 05:33:21 -0800

    2-largets_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,27 +5,25 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: 0
+ * Return: largest
  */
 
 int largest_number(int a, int b, int c)
 {
-	int largest_number;
+	int largest;
 
 	if (a > b && b > c)
 	{
-		largest_number = a;
+		largest = a;
 	}
 	else if (b > a && a > c)
 	{
-		largest_number = b;
+		largest = b;
 	}
 	else
 	{
-		largest_number = c;
+		largest = c;
 	}
-	printf("%d is the largest number\n", largest_number);
-
-	return (0);
+	return (largest);
 }
 
