9d2a7db7cf10b10f4a5d877a56af66e09ac75929
adedex <adedex1@gmail.com>
2022-12-19 10:45:23 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - swaps the values of two integers.
+ * pointer points to both numbers
+ * @a: pointer to the first number
+ * @b: pointer to the second number to update
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
