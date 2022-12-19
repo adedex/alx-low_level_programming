15b06093b8723160d5ad3899fa11d4b68c937bcf
adedex <adedex1@gmail.com>
2022-12-19 12:16:53 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 
 /**
  * _strlen - returns the length of a string
- * @s - string to evaluate
+ * @s:  string to evaluate
  * Return: Always 0
  */
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'); 
+	i++;
 	return (i);
 }
