9944508e6b4acc82cc0204a6285fe38b13cf3a61
adedex <adedex1@gmail.com>
2023-01-10 07:38:00 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,9 @@ char *create_array(unsigned int size, char c)
 	char *my_array;
 	unsigned int i = 0;
 
+	if (size == 0)
+		return ('\0');
+
 	my_array = malloc(size * sizeof(c));
 
 	while (i < size)
